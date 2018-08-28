d1f69c97ebbddd9d2c560e4f4013941c3bdbe556
stefan.butura <stefan_butura@yahoo.com>
2018-08-28 11:34:30 +0300

    Fixed settings.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -787,15 +787,4 @@ $settings['entity_update_batch_size'] = 50;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$config_directories['sync'] = '../config/sync';
-$databases['default']['default'] = array (
-  'database' => 'judicial',
-  'username' => 'root',
-  'password' => 'root',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['install_profile'] = 'standard';
+
