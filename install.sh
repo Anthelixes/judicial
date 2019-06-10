c819058060ac7d1d61cce16639be346278a31b90
stefan.butura <stefan_butura@yahoo.com>
2019-06-10 11:58:22 +0300

    Clean up docker, robo, install.sh.

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
+# Get the full path to the directory containing this script.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd "$SCRIPT_DIR/web"
+cd "$SCRIPT_DIR"
 
-# Site:install command requires write permission on settings.php file
-chmod u+w sites/default/ sites/default/settings.php
-
-../vendor/bin/drush site:install config_installer -y
-../vendor/bin/drush user:password admin password
-
-# Site:install removes write permission after it's finished
-chmod u+w sites/default/ sites/default/settings.php
+./vendor/bin/robo sql:sync
+./vendor/bin/robo site:update
+./vendor/bin/robo site:develop
