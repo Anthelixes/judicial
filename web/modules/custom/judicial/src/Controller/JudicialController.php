ade7381565f6ad27a74bace749189cc2d951d8df
stefan.butura <stefan_butura@yahoo.com>
2019-06-18 11:50:07 +0300

    Work on menu.

---
+++ web/modules/custom/judicial/src/Controller/JudicialController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\judicial\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class JudicialController extends ControllerBase {
+
+  public function glossary() {
+    return [];
+  }
+
+  public function elearning() {
+    return [];
+  }
+
+}
